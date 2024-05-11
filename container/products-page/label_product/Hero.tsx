3a8b6a1fd79849d1a97fa878cb10112e2acb7b2b
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-05-11 14:42:33 +0500

    modified

--- container/products-page/label_product/Hero.tsx
+++ container/products-page/label_product/Hero.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import { banner1 } from "@/public";
+import { Navbar } from "@/components";
 
 export default function Hero() {
-  return (
-    <section className="w-full padding-x py-10">
-      <Image src={banner1}
-        alt="vp660_banner"
-        className="w-full h-full object-cover"
-      />
-    </section>
-  )
+	return (
+		<section className="w-full min-h-screen flex flex-col justify-between items-center pb-10">
+			<Navbar />
+			<div className="padding-x">
+				<Image
+					src={banner1}
+					alt="vp660_banner"
+					className="w-full h-full object-cover"
+				/>
+			</div>
+		</section>
+	);
 }
